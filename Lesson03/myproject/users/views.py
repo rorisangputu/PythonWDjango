e986904564e73340e34831d899e976b8cc18eff0
Rorisang Putu <rorisangputu@gmail.com>
2025-01-27 12:44:07 +0200

    adding login functionality after registration. Creating new user

--- Lesson03/myproject/users/views.py
+++ Lesson03/myproject/users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:   
         form = UserCreationForm()
@@ -17,6 +18,7 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             #Login here
+            login(request, form.get_user())
             return redirect("posts:list")
     else:
         form = AuthenticationForm()
