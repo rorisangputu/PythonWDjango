fa03f7301a8f1b8ad066c629364fcdc9828520fd
Rorisang Putu <rorisangputu@gmail.com>
2025-01-27 13:54:01 +0200

    creating logout functionality

--- Lesson03/myproject/users/views.py
+++ Lesson03/myproject/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
@@ -23,3 +23,8 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form} )
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("posts:list")
